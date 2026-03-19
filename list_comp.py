cb06c2669f89121b6c2b289c001da36043d602d4
Ayanacyan <amitpacl@gmail.com>
2026-03-19 18:58:16 -0500

    List comprehension

---
+++ list_comp.py
@@ -0,0 +1,6 @@
+fruits=['Apple','Banana','Pear','Grapes','Rasberry']
+
+
+first_letters = list(map(lambda x: x[0], fruits))
+print('List of Capital First letters of the above list', first_letters)
+
